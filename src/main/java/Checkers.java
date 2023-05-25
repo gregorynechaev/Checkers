92fbf5dd1d645a15cec54a0f9d2c26e8d5993be0
Gregory <mpika1@yandex.ru>
2023-05-26 01:24:26 +0300

    aaa

--- src/main/java/Checkers.java
+++ src/main/java/Checkers.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 
-
 public class Checkers extends JPanel {
 
     public static void main(String[] args) {
@@ -13,7 +12,7 @@ public class Checkers extends JPanel {
         window.setContentPane(cont);
         window.pack();
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-        window.setLocation((size.width - window.getWidth())/2, (size.height - window.getHeight())/2 );
+        window.setLocation((size.width - window.getWidth()) / 2, (size.height - window.getHeight()) / 2);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
 
@@ -22,8 +21,6 @@ public class Checkers extends JPanel {
     private JButton play;
     private JButton finish;
     private JLabel label;
-
-
 
 
     public Checkers() {
@@ -38,7 +35,7 @@ public class Checkers extends JPanel {
         add(finish);
         add(label);
 
-        board.setBounds(40,40,328,328);
+        board.setBounds(40, 40, 328, 328);
         play.setBounds(400, 40, 280, 30);
         finish.setBounds(400, 100, 280, 30);
         label.setBounds(400, 200, 280, 200);
@@ -65,18 +62,19 @@ public class Checkers extends JPanel {
         int current;
 
         move[] available;
+        int selectRow, selectColumn;
 
 
-         Board() {
-             addMouseListener(this);
-             setBackground(Color.BLACK);
-             play = new JButton("Start a game");
-             finish = new JButton("Finish a game");
-             label = new JLabel("WELCOME!", JLabel.CENTER);
-             label.setForeground(new Color(47, 79, 79));
-             board = new CheckersInfo();
+        Board() {
+            addMouseListener(this);
+            setBackground(Color.BLACK);
+            play = new JButton("Start a game");
+            finish = new JButton("Finish a game");
+            label = new JLabel("WELCOME!", JLabel.CENTER);
+            label.setForeground(new Color(47, 79, 79));
+            board = new CheckersInfo();
 
-         }
+        }
 
         public void actionPerformed(ActionEvent evt) {
             Object src = evt.getSource();
@@ -87,25 +85,33 @@ public class Checkers extends JPanel {
         }
 
         void start() {
-             if(gameOn){
-                 label.setText("Firstly, finish your game!");
-                 return;
-             }
+            if (gameOn) {
+                label.setText("Firstly, finish your game!");
+                return;
+            }
 
-             board.ready();
-             current = CheckersInfo.LIGHT;
-             label.setText("White - It's your turn!");
+            board.ready();
+            current = CheckersInfo.LIGHT;
+            label.setText("White - It's your turn!");
             gameOn = true;
             play.setEnabled(false);
             finish.setEnabled(true);
 
         }
 
-        void finish(){
-             if(current == CheckersInfo.LIGHT) label.setText("Game over. RED wins");
-             else label.setText("Game over. WHITE wins");
+        void finish() {
+            if (current == CheckersInfo.LIGHT) label.setText("Game over. RED wins");
+            else label.setText("Game over. WHITE wins");
         }
 
+
+        void cellClick(int row, int column) {
+            selectColumn = column;
+            selectRow = row;
+            if (current == CheckersInfo.LIGHT) label.setText("WHITE, it's your turn");
+            else label.setText("RED, it's your turn");
+            return;
+        }
 
 
         public void mousePressed(MouseEvent evt) {
@@ -115,16 +121,21 @@ public class Checkers extends JPanel {
                 int column = (evt.getX() - 4) / 40;
                 int row = (evt.getY() - 4) / 40;
                 if (column >= 0 && column < 8 && row >= 0 && row < 16)
-                    label.setText("aaaaaaa");
+                    cellClick(row, column);
             }
         }
 
-        public void mouseReleased(MouseEvent evt) { }
-        public void mouseClicked(MouseEvent evt) { }
-        public void mouseEntered(MouseEvent evt) { }
-        public void mouseExited(MouseEvent evt) { }
+        public void mouseReleased(MouseEvent evt) {
+        }
 
+        public void mouseClicked(MouseEvent evt) {
+        }
 
+        public void mouseEntered(MouseEvent evt) {
+        }
+
+        public void mouseExited(MouseEvent evt) {
+        }
 
 
         public void paintComponent(Graphics g) {
@@ -138,7 +149,7 @@ public class Checkers extends JPanel {
                         g.setColor(new Color(139, 69, 19));
                     g.fillRect(4 + column * 40, 4 + row * 40, 40, 40);
 
-                    switch (board.pieceAt(row,column)) {
+                    switch (board.pieceAt(row, column)) {
                         case CheckersInfo.LIGHT:
                             g.setColor(Color.WHITE);
                             g.fillOval(8 + column * 40, 8 + row * 40, 30, 30);
@@ -152,14 +163,13 @@ public class Checkers extends JPanel {
             }
 
             g.setColor(Color.black);
-            g.drawRect(0,0,getSize().width-1,getSize().height-1);
-            g.drawRect(1,1,getSize().width-3,getSize().height-3);
+            g.drawRect(0, 0, getSize().width - 1, getSize().height - 1);
+            g.drawRect(1, 1, getSize().width - 3, getSize().height - 3);
+
+
         }
 
     }
-
-
-
 
 
     private static class CheckersInfo {
@@ -180,15 +190,14 @@ public class Checkers extends JPanel {
         void ready() {
             for (int row = 0; row < 8; row++) {
                 for (int column = 0; column < 8; column++) {
-                    if ( row % 2 == column % 2 ) {
+                    if (row % 2 == column % 2) {
                         if (row < 3)
                             board[row][column] = DARK;
                         else if (row > 4)
                             board[row][column] = LIGHT;
                         else
                             board[row][column] = EMPTY;
-                    }
-                    else {
+                    } else {
                         board[row][column] = EMPTY;
                     }
                 }
@@ -200,9 +209,6 @@ public class Checkers extends JPanel {
             return board[row][column];
         }
     }
-
-
-
 
 
 }
